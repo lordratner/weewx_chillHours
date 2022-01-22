688572eddc172a1b494c6d9b2fdcd608eb8670fd
Tom Keffer <tkeffer@gmail.com>
2022-01-22 05:36:50 -0800

    Create a new unit group, group_duration. Simplify.

--- chillTime.py
+++ chillTime.py
@@ -24,7 +24,8 @@ To use:
     5. Restart weewxd
 
 """
-import math
+
+import logging
 
 import weewx
 import weewx.units
@@ -32,10 +33,11 @@ import weewx.xtypes
 from weewx.engine import StdService
 from weewx.units import ValueTuple
 
-import logging
-
-# Tell the unit system what group our new observation type, 'chillTime', belongs to:
-weewx.units.obs_group_dict['chillTime'] = "group_elapsed"
+# Create a new unit group, 'group_duration', and assign chillTime to it.
+weewx.units.obs_group_dict['chillTime'] = "group_duration"
+weewx.units.USUnits['group_duration'] = 'hour'
+weewx.units.MetricUnits['group_duration'] = 'hour'
+weewx.units.MetricWXUnits['group_duration'] = 'hour'
 
 log = logging.getLogger(__name__)
 
@@ -46,22 +48,14 @@ class ChillTime(weewx.xtypes.XType):
         # Save the algorithm to be used.
         self.algorithm = algorithm.lower()
 
-    sql_stmts = {
-###*** Need to make sure the sqlLite statement is correct, since I don't use it
-        'sqlite': "SELECT outTemp FROM {table} WHERE dateTime BETWEEN {start} AND {stop}",
-        'mysql': "SELECT outTemp FROM {table} WHERE dateTime BETWEEN {start} AND {stop}"
-    }
-
     def get_scalar(self, obs_type, record, db_manager):
-        # Calculate 'chillTime'. For everything else, raise an exception UnknownType
+        """Calculate 'chillTime'. For everything else, raise an exception UnknownType"""
+
         if obs_type != 'chillTime':
             raise weewx.UnknownType(obs_type)
 
         # We need outTemp and interval in order to do the calculation.
-        if 'outTemp' not in record or record['outTemp'] is None:
-            raise weewx.CannotCalculate(obs_type)
-        if 'interval' not in record or record['interval'] is None:
-            raise weewx.CannotCalculate(obs_type)
+        if record.get('outTemp') is None or record.get('interval') is None:
 
         # We have everything we need. Start by forming a ValueTuple for the outside temperature and archive interval.
         # To do this, figure out what unit and group the outTemp record is in
@@ -79,60 +73,57 @@ class ChillTime(weewx.xtypes.XType):
         # Get the first element of the ValueTuple. This will be in Celsius:
         outTemp_F = outTemp_F_vt[0]
         interval_H = interval_H_vt[0]
-        
+
         if self.algorithm == 'simple':
             # Use the "Simple" algorithm. Results will be in hours. Create a ValueTuple out of it:
             if outTemp_F < 45:
                 chill_time = interval_H
             else:
                 chill_time = 0
-            chill_vt = ValueTuple(chill_time, 'hour', 'group_elapsed')
             log.debug("Found Simple chill hours of %s hours", chill_time)
         elif self.algorithm == 'utah':
             # Use the "Utah" algorithm, weighting chill temps between 32 and 45F
             if outTemp_F <= 34:
                 chill_time = 0
-            elif 34 < outTemp_F and outTemp_F <= 36:
+            elif 34 < outTemp_F <= 36:
                 chill_time = interval_H * .5
-            elif 36 < outTemp_F and outTemp_F <= 48:
+            elif 36 < outTemp_F <= 48:
                 chill_time = interval_H
-            elif 48 < outTemp_F and outTemp_F <= 54:
+            elif 48 < outTemp_F <= 54:
                 chill_time = interval_H * .5
-            elif 54 < outTemp_F and outTemp_F <= 60:
+            elif 54 < outTemp_F <= 60:
                 chill_time = 0
-            elif 60 < outTemp_F and outTemp_F <= 65:
+            elif 60 < outTemp_F <= 65:
                 chill_time = interval_H * -.5
             elif 65 < outTemp_F:
                 chill_time = interval_H * -1
-            chill_vt = ValueTuple(chill_time, 'hour', 'group_elapsed')
+            else:
+                chill_time = 0
             log.debug("Found Utah chill hours of %s hours", chill_time)
         elif self.algorithm == 'modified':
             # Use the "Modified" algorithm, counting only chill temps between 32 and 45F
-            if outTemp_F < 45 and 32 < outTemp_F:
+            if 32 < outTemp_F < 45:
                 chill_time = interval_H
             else:
                 chill_time = 0
-            chill_vt = ValueTuple(chill_time, 'hour', 'group_elapsed')
             log.debug("Found Modified chill hours of %s hours", chill_time)
         else:
-            # Don't recognize the exception. Fail hard:
-            raise ValueError(self.algorithm)
+            # Don't recognize the algorithm type. Fail hard:
+            raise ValueError("Unrecognized chill time algorithm '%s'" % self.algorithm)
 
-        # We have the chill hours as a ValueTuple. Convert it back to the units used by the incoming record and return it
-###*** Converting back to the schema units turns it into seconds. For now this conflicts with the image generators that can't convert the units to hours. When fixed, this can be uncommented and the temp solution removed
-        #return weewx.units.convertStd(chill_vt, record['usUnits'])
+        # Form a ValueTuple out of our results and return it
+        chill_vt = ValueTuple(chill_time, 'hour', 'group_elapsed')
+
         return chill_vt
 
     def get_aggregate(self, obs_type, timespan, aggregate_type, db_manager, **option_dict):
 
-        dbtype = db_manager.connection.dbtype
-        
         if obs_type != 'chillTime':
             raise weewx.UnknownType(obs_type)
         if aggregate_type != 'sum':
             raise weewx.UnknownAggregation(aggregate_type)
 
-###*** Needs to be moved to after the sql call, not sure how to make it exit the 'for' loop
+        ###*** Needs to be moved to after the sql call, not sure how to make it exit the 'for' loop
         '''
         if 'outTemp' not in record or record['outTemp'] is None:
             raise weewx.CannotCalculate(obs_type)
@@ -146,88 +137,14 @@ class ChillTime(weewx.xtypes.XType):
             'stop': timespan.stop
         }
 
-        sql_stmt = ChillTime.sql_stmts[dbtype].format(**interp_dict)
-
-        try:
-            row_gen = db_manager.genBatchRecords(timespan.start, timespan.stop)
-        except:
-            log.error("weedb No Column Error rasied")
-
-###*** Need the correct error hanlding for genBatchRecords here. No clue what it should be   
-        """
-        except weedb.NoColumnError:
-            log.error("weedb No Column Error rasied")
-            raise weewx.UnknownType(aggregate_type)
-
-        if not row or None in row:
-            value = None"""
-
         chill_total = 0
-        
-###*** Need better way to get usUnits. Pass from ChillTimeService from config_dict?
-        vt_sql = 'SELECT usUnits FROM archive ORDER BY dateTime DESC LIMIT 1'
-        vt_units = db_manager.getSql(vt_sql)[0]
-        
-        
-        for record in row_gen:
-            vt_units = record['usUnits']
-            # We have everything we need. Start by forming a ValueTuple for the outside temperature and archive interval.
-            # To do this, figure out what unit and group the outTemp record is in
-            unit_and_group = weewx.units.getStandardUnitType(vt_units, 'outTemp')
-            # ... then form the ValueTuple.
-            outTemp_vt = ValueTuple(record['outTemp'], *unit_and_group)
-            # Now do it for the interval
-            unit_and_group = weewx.units.getStandardUnitType(vt_units, 'interval')
-            interval_vt = ValueTuple(record['interval'], *unit_and_group)
 
-            # Both algorithms need temperature in Farenheit, so let's make sure our incoming temperature
-            # is in that unit. Use function convert(). The results will be in the form of a ValueTuple
-            outTemp_F_vt = weewx.units.convert(outTemp_vt, 'degree_F')
-            interval_H_vt = weewx.units.convert(interval_vt, 'hour')
-            # Get the first element of the ValueTuple. This will be in Celsius:
-            outTemp_F = outTemp_F_vt[0]
-            interval_H = interval_H_vt[0]
-            
-            if self.algorithm == 'simple' and outTemp_F != None:
-                # Use the "Simple" algorithm. Results will be in hours. Create a ValueTuple out of it:
-                if outTemp_F < 45:
-                    chill_time = interval_H
-                else:
-                    chill_time = 0
-            elif self.algorithm == 'utah' and outTemp_F != None:
-                # Use the "Utah" algorithm, weighting chill temps between 32 and 45F
-                if outTemp_F <= 34:
-                    chill_time = 0
-                elif 34 < outTemp_F and outTemp_F <= 36:
-                    chill_time = interval_H * .5
-                elif 36 < outTemp_F and outTemp_F <= 48:
-                    chill_time = interval_H
-                elif 48 < outTemp_F and outTemp_F <= 54:
-                    chill_time = interval_H * .5
-                elif 54 < outTemp_F and outTemp_F <= 60:
-                    chill_time = 0
-                elif 60 < outTemp_F and outTemp_F <= 65:
-                    chill_time = interval_H * -.5
-                elif 65 < outTemp_F:
-                    chill_time = interval_H * -1
-            elif self.algorithm == 'modified' and outTemp_F != None:
-                # Use the "Modified" algorithm, counting only chill temps between 32 and 45F
-                if outTemp_F < 45 and 32 < outTemp_F:
-                    chill_time = interval_H
-                else:
-                    chill_time = 0
-            # Not sure the 'right' way to catch a None that sneaks in here, but it's popping up
-            elif outTemp_F == None:
-                chill_time = 0
-            else:
-                # Don't recognize the exception. Fail hard:
-                raise ValueError(self.algorithm)
-            chill_total += chill_time
+        for record in db_manager.getBatchRecords(*timespan):
+            chill_delta = self.get_scalar('chillTime', record, db_manager)
+            chill_total += chill_delta
+
         chill_vt = ValueTuple(chill_total, 'hour', 'group_elapsed')
 
-        # We have the chill hours as a ValueTuple. Convert it back to the units used by the incoming record and return it        
-###*** Converting back to the schema units turns it into seconds. For now this conflicts with the image generators that can't convert the units to hours. When fixed, this can be uncommented and the temp solution removed
-        #return weewx.units.convertStd(chill_vt, record['usUnits'])
         return chill_vt
 
 
